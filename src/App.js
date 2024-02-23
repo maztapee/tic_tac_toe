92e798ff31b31f083f25cde896e57955e245d9e1
maztapee <91094826+maztapee@users.noreply.github.com>
2024-02-23 10:35:13 +0100

    (generated by blackbox.ai )

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  function Square({value}){
+    function clickSquare(){
+      setValue('X');
+    };
+    const [value, setValue] = useState(null);
+    return <button className="square" onClick={clickSquare}> {value} </button>
+  };
+
+  function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    return <>
+      <div className="board-row">
+        <Square value={squares[0]}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
+      </div>
+
+      <div className="board-row">
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
+      </div>
+
+      <div className="board-row">
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
+      </div>
+
+    </>
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First React App Remastering the Tic-Tac-Toe Demo.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="board">
+          < Board/>
+        </div>
       </header>
     </div>
   );
