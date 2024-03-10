1e7eda3b8884325aac1553a09aba246e8fa975c5
maztapee <91094826+maztapee@users.noreply.github.com>
2024-03-10 10:45:29 +0100

    Full Working Version Implemented from React Official Tutorial

--- src/App.js
+++ src/App.js
@@ -1,58 +1,126 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
-function Square (){
-  const [value, setValues] = useState(null);
-  function handleClick(){
-    setValues('X');
-  };
+function Square({ value, onSquareClick }) {
+  return (
+    <div className='square-row'>
+      <button className='squares' onClick={onSquareClick}>
+        {value}
+      </button>
+    </div>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if(xIsNext){
+      nextSquares[i] = "X";
+    } else{
+      nextSquares[i] = "O"; 
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
   return (
     <>
-    <div className='square-row'>
-      <button className='squares' onClick={handleClick}> {value} </button>
-    </div>
+      <div className="status">{status}</div>
+      <div className='square-row-box'>
+        {[0, 1, 2].map((i) => (
+          <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+      <div className='square-row-box'>
+        {[3, 4, 5].map((i) => (
+          <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+      <div className='square-row-box'>
+        {[6, 7, 8].map((i) => (
+          <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
     </>
-  )
-};
+  );
+}
 
-function Board (){
-  return(
+export default function TicTacToe() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory= [...history.slice(0, currentMove + 1),  nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move > 0){
+      description = 'Go to move #' + move;
+    } else{
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
     <>
-    <div className='square-row-box'>
-      < Square />
-      < Square />
-      < Square />
-    </div>
-
-    <div className='square-row-box'>
-      < Square />
-      < Square />
-      < Square />
-    </div>
-
-    <div className='square-row-box'>
-      < Square />
-      < Square />
-      < Square />
-    </div>
-  </>
-  )
-};
-
-// --------------------------------------ExPoRt ApP hErE--------------------------------------------------------------
-export default function tic_tac_toe(){
-  return(  
-  <>
       <div className='react-logo'>
         <h1 className="title">My Tic Tac Toe Game</h1>
       </div>
-      <img className="App-logo" src= {logo} alt="App-logo" />
+      <img className="App-logo" src={logo} alt="App-logo" />
       <div className='board-box'>
-        <Board/>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-  </>
-  
-  )
-};
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
